821d3a52bccd9d50feba97ef49a1bd5bf009fd13
79Aniro <capablanca1@bol.com.br>
2018-09-20 15:07:44 -0300

    Navegando entre paginas

--- Ionic 3 para Iniciantes/workspace/ionic/myapp/src/pages/intro/intro.ts
+++ Ionic 3 para Iniciantes/workspace/ionic/myapp/src/pages/intro/intro.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
-/**
- * Generated class for the IntroPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -20,6 +16,11 @@ export class IntroPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IntroPage');
+  }
+
+  goToTabsPage(){
+
+    this.navCtrl.push(TabsPage);
   }
 
 }
