2cc9d0f795a6a71b6a97b74b0d7a29c0edb4ad4b
79Aniro <capablanca1@bol.com.br>
2018-11-18 17:48:53 -0200

    Exibindo a Imagem na Tela

--- Ionic 3 - Recursos Nativos para IOS e Android/work/modelo1/src/pages/home/home.ts
+++ Ionic 3 - Recursos Nativos para IOS e Android/work/modelo1/src/pages/home/home.ts
@@ -8,17 +8,24 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  base64Image:string;
   constructor(public navCtrl: NavController,
     private camera: Camera) {
 
   }
 
   abrirCamera(){
-    this.camera.getPicture().then((imageData) => {
+
+    let config:CameraOptions={
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(config).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64 (DATA_URL):
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
      }, (err) => {
       // Handle error
      });
