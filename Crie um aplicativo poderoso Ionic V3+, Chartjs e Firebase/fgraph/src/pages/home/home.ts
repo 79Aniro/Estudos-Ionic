50d102e0c693446fc060dd32a523e6d1d23dbb93
79Aniro <capablanca1@bol.com.br>
2019-04-28 17:04:45 -0300

    Definindo os atributos da classe

--- Crie um aplicativo poderoso Ionic V3+, Chartjs e Firebase/fgraph/src/pages/home/home.ts
+++ Crie um aplicativo poderoso Ionic V3+, Chartjs e Firebase/fgraph/src/pages/home/home.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController,NavParams } from 'ionic-angular';
-import{AngularFireDatabase} from 'angularfire2/database';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController,NavParams,LoadingController } from 'ionic-angular';
+import{AngularFireDatabase, AngularFireObject} from 'angularfire2/database';
 
 
 import{Observable} from 'rxjs/Observable';
 
 import{AngularFireList} from 'angularfire2/database';
 
-import{ToastController} from 'ionic-angular/components/toast/toast-controller'
+import{ToastController} from 'ionic-angular/components/toast/toast-controller';
+
+import{Chart} from 'chart.js';
 
 @Component({
   selector: 'page-home',
@@ -15,7 +17,25 @@ import{ToastController} from 'ionic-angular/components/toast/toast-controller'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data:Observable<any[]>;
+  ref: AngularFireList<any>;
+  refMeta:AngularFireObject<any>;
+
+  unidades=[
+    {value:"unidade1",name:"Unidade ES"},
+    {value:"unidade2",name:"Unidade SP"},
+    {value:"unidade3",name:"Unidade RJ"},
+    {value:"unidade4",name:"Unidade BA"}
+  ]
+//Graficos
+  @ViewChild ('graficoCanvas1') graficoCanvas1;
+  @ViewChild ('graficoCanvas2') graficoCanvas2;
+
+  objChartJs:any;
+  ObjChartJs2:any;
+  constructor(public navCtrl: NavController,
+  private db:AngularFireDatabase,
+  public loadingCtrl:LoadingController) {
 
   }
 
