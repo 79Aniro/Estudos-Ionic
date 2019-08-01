f4e32144661983d93eeb817b8c10d4ff0ff20530
79Aniro <capablanca1@bol.com.br>
2019-08-01 10:05:06 -0300

    Viaturas app

---
+++ Viaturas/src/modelos/viatura.ts
@@ -0,0 +1,15 @@
+export interface ViaturaDTO{
+
+    placa:string;
+    patrimonio:string;
+    kilometragem:number;
+    modelo:string;
+    anoFabricacao:number;
+    ultimaRevisao:Date;
+    ultimaRevisaoKm:number;
+    proximaRevisaoKm:number;
+    proximaRevisao:Date;
+    ultimaTrocaPneus:Date;
+    proximaTrocaPneus:Date;
+
+}
